cbdd65044538945b81cd6654c28e6dc102f59693
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-20 15:46:30 +0600

    BarChart complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import LineCharts from './components/LineCharts';
+import BarChart from './components/BarChart';
 
 function App() {
   return (
     <div className="App">
       <LineCharts></LineCharts>
+      <br/>
+      <BarChart></BarChart>
     </div>
   );
 }
