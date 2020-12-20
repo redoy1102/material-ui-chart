5e92a2db13d211e947f24dacd1205275ff650f08
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-20 15:37:47 +0600

    Complete line chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LineCharts from './components/LineCharts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+      <LineCharts></LineCharts>
     </div>
   );
 }
